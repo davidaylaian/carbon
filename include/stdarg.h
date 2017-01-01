8bfa3d471e40ed3d963cdda8ed3db91cfe478de4
David Aylaian <DavidAylaian@users.noreply.github.com>
2017-01-01 18:33:26 -0500

    Added stdarg.h to library

---
+++ include/stdarg.h
@@ -0,0 +1,25 @@
+#ifndef STDARG_H
+#define STDARG_H
+
+#include <stdint.h>
+
+typedef uint8_t*	va_list;
+
+#define STACKITEM	sizeof(int)
+
+#define	VA_SIZE(TYPE)			\
+	((sizeof(TYPE) + STACKITEM - 1)	\
+	& ~(STACKITEM - 1))		\
+
+#define	va_start(AP, LASTARG)		\
+	(AP=((va_list)&(LASTARG)	\
+	+ VA_SIZE(LASTARG)))		\
+
+#define va_end(AP)
+
+#define va_arg(AP, TYPE)		\
+	(AP += VA_SIZE(TYPE),		\
+	*((TYPE *)			\
+	(AP - VA_SIZE(TYPE))))		\
+
+#endif
