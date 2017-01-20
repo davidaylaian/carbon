32cff4e204d5bcc6a8fd605658d5a7e9487dfcdd
David Aylaian <DavidAylaian@users.noreply.github.com>
2017-01-19 21:07:30 -0500

    Modified strlen implementation

--- library/string/strlen.c
+++ library/string/strlen.c
@@ -1,9 +1,9 @@
 #include <string.h>
 
-size_t strlen(string str)
+// returns the length of str
+size_t strlen(const string str)
 {
 	size_t length = 0;
 	for(; str[length] != 0; length++);
-	
 	return length;
 }
