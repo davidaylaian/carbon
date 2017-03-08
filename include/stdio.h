063582938044c67ce2b35583f00bb031007935cc
David Aylaian <DavidAylaian@users.noreply.github.com>
2017-03-08 15:53:42 -0500

    Removed sprint(), sprintln(), and sprintf() stubs
    I will add them eventually but they're too low on my priorites for the project

--- include/stdio.h
+++ include/stdio.h
@@ -14,9 +14,4 @@ extern void print(string str);
 extern void printf(string format, ...);
 extern void println(string str);
 
-// print to string
-extern string sprint(string ret, string str);
-extern string sprintf(string ret, string format, ...);
-extern string sprintln(string ret, string str);
-
 #endif
