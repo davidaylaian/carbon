cfe12ece864f799472f44454c6954db90100c4e1
David Aylaian <DavidAylaian@users.noreply.github.com>
2017-03-14 00:19:02 -0400

    Added vprintfln() and printfln() to stdio.h

--- include/stdio.h
+++ include/stdio.h
@@ -19,4 +19,21 @@ inline void println(string str) {
 	printf("%s\n", str);
 }
 
+// prints a va_list argument list in the given format and a newline
+inline void vprintfln(string format, va_list args) {
+	vprintf(format, args);
+	print("\n");
+}
+
+// prints an ellipsis argument list in the given format and a newline
+inline void printfln(string format, ...)
+{
+	va_list args;
+	va_start(args, format);
+	vprintf(format, args);
+	va_end(args);
+	
+	print("\n");
+}
+
 #endif
