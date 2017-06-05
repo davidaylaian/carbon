8a63b90e4e13dcccd09e78a68947c6136a13cb20
David Aylaian <DavidAylaian@users.noreply.github.com>
2017-06-04 21:33:48 -0400

    Cleaned up code

--- arch/x86/asm.c
+++ arch/x86/asm.c
@@ -14,12 +14,12 @@ void disable() {
 uint8_t inb(uint16_t port)
 {
 	uint8_t val;
-	asm volatile ("inb %0, %1" : "=a"(val): "Nd"(port));
+	asm volatile("inb %0, %1" : "=a"(val) : "Nd"(port));
 	return val;
 }
 
 // send an 8-bit value to an I/O device
 void outb(uint16_t port, uint8_t val)
 {
-	asm volatile ("outb %1, %0" : : "a"(val), "Nd"(port));
+	asm volatile("outb %1, %0" :: "a"(val), "Nd"(port));
 }
