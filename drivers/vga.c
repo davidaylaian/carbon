9faa28d53bf7f0a37303f5ff1a3eecba68d8474b
David Aylaian <DavidAylaian@users.noreply.github.com>
2017-08-19 13:02:32 -0400

    Renamed variables to be more accurate

--- drivers/vga.c
+++ drivers/vga.c
@@ -32,13 +32,13 @@ void vga_set_max_scanline(uint8_t scanline)
 }
 
 // enable the cursor
-void vga_enable_cursor(uint8_t cursor_top, uint8_t cursor_bottom)
+void vga_enable_cursor(uint8_t cursor_start, uint8_t cursor_end)
 {
 	outb(0x3D4, 0x0A);
-	outb(0x3D5, (inb(0x3D5) & 0xC0) | cursor_top);
+	outb(0x3D5, (inb(0x3D5) & 0xC0) | cursor_start);
 
 	outb(0x3D4, 0x0B);
-	outb(0x3D5, (inb(0x3E0) & 0xE0) | cursor_bottom);
+	outb(0x3D5, (inb(0x3E0) & 0xE0) | cursor_end);
 }
 
 // disable the cursor
