5dce3b05bdfe0401b74a9ed7e71a94e2be3a72e1
David Aylaian <davidaylaian@protonmail.com>
2017-08-29 10:23:19 -0400

    Made irq.c compliant with new coding standards

--- arch/i386/irq.c
+++ arch/i386/irq.c
@@ -32,22 +32,22 @@ extern void irq_wrapper_15();
 // install irqs into their corresponding slots in the idt
 void irq_install()
 {
-	setvect(32, (unsigned)irq_wrapper_0);
-	setvect(33, (unsigned)irq_wrapper_1);
-	setvect(34, (unsigned)irq_wrapper_2);
-	setvect(35, (unsigned)irq_wrapper_3);
-	setvect(36, (unsigned)irq_wrapper_4);
-	setvect(37, (unsigned)irq_wrapper_5);
-	setvect(38, (unsigned)irq_wrapper_6);
-	setvect(39, (unsigned)irq_wrapper_7);
-	setvect(40, (unsigned)irq_wrapper_8);
-	setvect(41, (unsigned)irq_wrapper_9);
-	setvect(42, (unsigned)irq_wrapper_10);
-	setvect(43, (unsigned)irq_wrapper_11);
-	setvect(44, (unsigned)irq_wrapper_12);
-	setvect(45, (unsigned)irq_wrapper_13);
-	setvect(46, (unsigned)irq_wrapper_14);
-	setvect(47, (unsigned)irq_wrapper_15);
+	setvect(32, (unsigned) irq_wrapper_0);
+	setvect(33, (unsigned) irq_wrapper_1);
+	setvect(34, (unsigned) irq_wrapper_2);
+	setvect(35, (unsigned) irq_wrapper_3);
+	setvect(36, (unsigned) irq_wrapper_4);
+	setvect(37, (unsigned) irq_wrapper_5);
+	setvect(38, (unsigned) irq_wrapper_6);
+	setvect(39, (unsigned) irq_wrapper_7);
+	setvect(40, (unsigned) irq_wrapper_8);
+	setvect(41, (unsigned) irq_wrapper_9);
+	setvect(42, (unsigned) irq_wrapper_10);
+	setvect(43, (unsigned) irq_wrapper_11);
+	setvect(44, (unsigned) irq_wrapper_12);
+	setvect(45, (unsigned) irq_wrapper_13);
+	setvect(46, (unsigned) irq_wrapper_14);
+	setvect(47, (unsigned) irq_wrapper_15);
 }
 
 // function pointers to irq handlers
