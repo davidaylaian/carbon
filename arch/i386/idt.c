aca24efd13b407065021cad800f6bab1515d023c
David Aylaian <davidaylaian@protonmail.com>
2017-08-29 10:03:03 -0400

    Made idt.c compliant with new coding standards

--- arch/i386/idt.c
+++ arch/i386/idt.c
@@ -11,27 +11,24 @@
 
 // describes an interrupt descriptor
 struct idt_descriptor {
-	uint16_t  baseLo;
-	uint16_t  selector;
-	uint8_t   reserved;
-	uint8_t   flags;
-	uint16_t  baseHi;
+	uint16_t baseLo;
+	uint16_t selector;
+	uint8_t  reserved;
+	uint8_t  flags;
+	uint16_t baseHi;
 } __attribute__((packed));
 
 // describes the structure for the idtr register
 struct idtr {
-	uint16_t  limit;
-	uint32_t  base;
+	uint16_t limit;
+	uint32_t base;
 } __attribute__((packed));
 
-// the interrupt descriptor table is an array of descriptors
-struct idt_descriptor idt[MAX_INTERRUPTS];
-
-// idtr is used to set the idtr register to point to the idt
+static struct idt_descriptor idt[MAX_INTERRUPTS];
 struct idtr idtr;
 
 // display (primitive) error message and halt execution
-noreturn void default_handler()
+noreturn static void default_handler()
 {
 	puts("\n");
 	puts("              +==================================================+              ");
@@ -44,7 +41,7 @@ noreturn void default_handler()
 	while (1);
 }
 
-// installs an interrupt routine (ir)
+// install an interrupt routine (ir)
 void install_ir(uint32_t i, uint64_t base, uint16_t flags, uint16_t sel)
 {
 	// set base address
@@ -59,15 +56,16 @@ void install_ir(uint32_t i, uint64_t base, uint16_t flags, uint16_t sel)
 	idt[i].selector	= sel;
 }
 
+// install the idt
 void idt_install()
 {
 	// set up idtr
-	idtr.limit = sizeof(struct idt_descriptor) * MAX_INTERRUPTS-1;
-	idtr.base  = (uint32_t) & idt[0];
+	idtr.limit = sizeof(struct idt_descriptor) * MAX_INTERRUPTS - 1;
+	idtr.base  = (uint32_t) &idt[0];
 
 	// register default handlers
-	for (size_t i=0; i<MAX_INTERRUPTS; i++)
-		install_ir (i, (size_t)default_handler, 0x8E, 0x08);
+	for (size_t i = 0; i < MAX_INTERRUPTS; i++)
+		install_ir(i, (size_t) default_handler, 0x8E, 0x08);
 
 	// load idt by setting idtr register to point to idt
 	asm("lidt [idtr]");
