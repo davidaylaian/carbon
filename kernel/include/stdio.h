76247a649c4413cd1ee548a7201e694faa9f581c
David Aylaian <davidaylaian@protonmail.com>
2020-04-06 11:44:57 -0400

    Use mpaland/printf library for *printf function family

--- kernel/include/stdio.h
+++ kernel/include/stdio.h
@@ -12,15 +12,27 @@
 #include <stdarg.h>
 #include <stddef.h>
 
-/*
- * TODO: Carbon will eventually need a more sophisticated buffer-based stdio.
- * However, it won't matter until we start getting into userspace. For now,
- * we just need something small and forwards compatible.
- */
-
 void putchar(char c);
 void puts(const char* str);
-int vprintf(const char* format, va_list args);
-int printf(const char* format, ...);
+
+/*
+ * For the *printf family, we use a library called mpaland/printf which can be found in libk/stdio/printf.c.
+ * The library names all of the functions with underscores to avoid conflicts.
+ */
+
+#define printf printf_
+#define sprintf sprintf_
+#define snprintf snprintf_
+#define vsnprintf vsnprintf_
+#define vprintf vprintf_
+
+int printf_(const char* format, ...);
+int sprintf_(char* buffer, const char* format, ...);
+int snprintf_(char* buffer, size_t count, const char* format, ...);
+int vsnprintf_(char* buffer, size_t count, const char* format, va_list va);
+int vprintf_(const char* format, va_list va);
+
+// use output function (instead of buffer) for streamlike interface
+int fctprintf(void (*out)(char character, void* arg), void* arg, const char* format, ...);
 
 #endif
